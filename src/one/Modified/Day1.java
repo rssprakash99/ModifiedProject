8abb41cd6d2ad638d97e1f508313b2461de81623
Prakash <rssprakash99@gmail.com>
2020-11-24 20:35:15 +0530

    Updated with Day1 and Day2 programs

---
+++ src/one/Modified/Day1.java
@@ -0,0 +1,10 @@
+package one.Modified;
+
+public class Day1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/one/Modified/Day2.java
@@ -0,0 +1,10 @@
+package one.Modified;
+
+public class Day2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
